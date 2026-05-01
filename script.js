a307183ec5091b27fd1a2546e2e5780ea9d54fba
AwesomeeDev <AwesomeDevv@proton.me>
2026-05-01 12:21:08 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,28 @@
 let code = "";
 
-// CLOCK
-setInterval(() => {
-  document.getElementById("clock").innerText =
-    new Date().toLocaleTimeString();
-}, 1000);
-
-// LOGIN
 function sendCode() {
   const email = document.getElementById("email").value;
 
   if (!email.endsWith("@rutherford.school.nz")) {
-    document.getElementById("error").innerText = "Invalid school email.";
+    document.getElementById("error").innerText =
+      "Use school email.";
     return;
   }
 
   code = Math.floor(100000 + Math.random() * 900000).toString();
 
-  console.log("CODE:", code);
   alert("Your code is: " + code);
 
-  document.getElementById("codeArea").style.display = "block";
+  document.getElementById("codeBox").style.display = "block";
 }
 
 function verifyCode() {
   const input = document.getElementById("codeInput").value;
 
   if (input === code) {
-    document.getElementById("login").style.display = "none";
-    document.getElementById("desktop").style.display = "block";
+    window.location.href = "desktop.html";
   } else {
-    document.getElementById("error").innerText = "Wrong code.";
+    document.getElementById("error").innerText =
+      "Incorrect code.";
   }
-}
-
-// WINDOWS
-function openApp(id) {
-  document.getElementById(id).style.display = "block";
-}
-
-function closeApp(id) {
-  document.getElementById(id).style.display = "none";
-}
-
-// LINKS
-function openMail() {
-  window.open("https://outlook.live.com", "_blank");
-}
-
-function openSearch() {
-  window.open("https://www.google.com", "_blank");
 }
